ff7d80d86e615fcd9d7733bab7e24564088cbebc
Daniel García Peña <danigarciape@gmail.com>
2026-04-20 08:31:19 +0200

    feat: dynamically import HomeClient for client-side rendering

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,4 +1,10 @@
-import { HomeClient } from '@/components/HomeClient'
+"use client"
+
+import dynamic from 'next/dynamic'
+
+const HomeClient = dynamic(() => import('@/components/HomeClient').then((mod) => mod.HomeClient), {
+  ssr: false,
+})
 
 export default function Home() {
   return <HomeClient />
